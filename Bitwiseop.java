689b9a6e77953b1791af368853657ddd596b6e03
yaseen97425 <109179754+yaseen97425@users.noreply.github.com>
2022-10-27 11:32:57 +0530

    Add files via upload

---
+++ Bitwiseop.java
@@ -0,0 +1,17 @@
+package Javaexamples;
+
+public class Bitwiseop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a=10,b=5;
+			System.out.println(" a & b : "+(a&b));
+			System.out.println(" a | b : "+(a|b));
+			System.out.println(" a ^ b : "+(a^b));
+			System.out.println(" ~a : "+(~a));
+			System.out.println(" ~b : "+(~b));
+
+
+	}
+
+}
