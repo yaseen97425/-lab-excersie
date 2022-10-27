90bfeda38063a89d668d8b6007723c31fcbfb7c4
yaseen97425 <109179754+yaseen97425@users.noreply.github.com>
2022-10-27 10:33:52 +0530

    Add files via upload

---
+++ incdecdemo.java
@@ -0,0 +1,20 @@
+package Javaexamples;
+
+public class incdecdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+						int val=10,val2=10;
+			// Incrementing both pre and post fix
+			System.out.println("Incrementing both pre and post fix");
+			System.out.println("Post Increment: " +val++);
+			System.out.println("Pre Increment: "+(++val));
+			
+			// Decrementing both pre and post fix
+					System.out.println("Decrementing both pre and post fix");
+					System.out.println("Post Decrement: " +val--);
+					System.out.println("Pre Decrement: "+(--val));
+		}
+
+}
