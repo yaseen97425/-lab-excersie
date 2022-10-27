a9cb5d35c15dcdbf1b95e9442f1f698c4c3cc070
yaseen97425 <109179754+yaseen97425@users.noreply.github.com>
2022-10-27 10:59:23 +0530

    Add files via upload

---
+++ operdemo.java
@@ -0,0 +1,18 @@
+package Javaexamples;
+
+public class operdemo {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int add,sub,mul,div,mod; //creating a integer variables
+			int num1=10,num2=10;  	 // Assign values to int variables 
+			add=num1+num2;
+			div=num1/num2;
+			sub=num1-num2;
+			mul=num1*num2;
+			mod=num1%num2;
+			
+			System.out.println(add+"\n"+sub+"\n"+mul+"\n"+div+"\n"+mod+"\n");
+		}
+
+	}
