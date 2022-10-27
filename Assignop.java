b33a78e0c02a3f6a830cd41169ac378c3f587a56
yaseen97425 <109179754+yaseen97425@users.noreply.github.com>
2022-10-27 11:31:40 +0530

    Add files via upload

---
+++ Assignop.java
@@ -0,0 +1,18 @@
+package Javaexamples;
+
+public class Assignop {
+	public static void main(String[] args) {	
+		// TODO Auto-generated method stub	
+		int a=10,b=10,c=10,d=10,e=10;		
+		System.out.println( a+=5 ); 
+		// a=a+5;		
+		System.out.println( b-=5 );	
+		// b=b-5;	
+		System.out.println( c*=5 );
+		// c=c*5;		
+		System.out.println( d/=5 );		
+		// d=d/5;		
+		System.out.println( e%=5 );
+	}
+
+}
