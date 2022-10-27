f421924c6db1467c8e77c7f95753703e2e9865ee
yaseen97425 <109179754+yaseen97425@users.noreply.github.com>
2022-10-27 12:11:17 +0530

    Add files via upload

---
+++ ternarydemo.java
@@ -0,0 +1,16 @@
+package Javaexamples;
+
+public class ternarydemo {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub	
+			int a=5,b=6;	
+			String res;		
+			res=(a==10)?"Equal":"Not Equal";
+			System.out.println(res);	
+			res=(b==10)?"Equal":"Not Equal";	
+			System.out.println(res);		
+			System.out.println(~a);
+	}
+
+}
